921ab76cb5f73a35363690b22624cdafcc869a37
RIIDLEY <hamsek.lahoucine@gmail.com>
2020-10-26 00:11:02 +0100

    Ajout sécu pour audio

--- app/src/main/java/com/example/drumpad/About.kt
+++ app/src/main/java/com/example/drumpad/About.kt
@@ -1,5 +1,6 @@
 package com.example.drumpad
 
+import android.media.AudioAttributes
 import android.media.AudioManager
 import android.media.MediaPlayer
 import android.net.Uri
@@ -8,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class About : AppCompatActivity() {
-
+    private lateinit var player: MediaPlayer
     private lateinit var mediaPlayer: MediaPlayer
     private val rtspUrl = "http://lahoucine-hamsek.site/Jasmin.mp3"
 
@@ -16,28 +17,35 @@ class About : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
 
-        mediaPlayer = MediaPlayer()
+        player = MediaPlayer.create(this@About, Uri.parse("http://lahoucine-hamsek.site/Jasmin.mp3"))
 
-        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC)
+        player.start()
 
-        mediaPlayer.setDataSource("http://lahoucine-hamsek.site/Jasmin.mp3")
 
-        mediaPlayer.prepare()
-
-//        mediaPlayer.setOnPreparedListener( MediaPlayer.OnPreparedListener() {
+//        mediaPlayer = MediaPlayer.create(this, R.raw.cymbal1)
+//
+//        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC)
+//
+//        mediaPlayer.setDataSource("http://lahoucine-hamsek.site/Jasmin.mp3")
+//
+//        mediaPlayer.prepare()
+//
+//        mediaPlayer.setOnPreparedListener(MediaPlayer.OnPreparedListener() {
 //            fun onPrepared(mp: MediaPlayer) {
 //                mp.start();
-//            }})
-
-        mediaPlayer.start()
+//            }
+//        })
+//
+//        mediaPlayer.start()
 
 //        val sampleUrl = "http://lahoucine-hamsek.site/Jasmin.mp3" // your URL here
 //        val mediaPlayer: MediaPlayer? = MediaPlayer().apply {
-//            setAudioStreamType(AudioManager.STREAM_MUSIC) //to send the object to the initialized state
+//           // setAudioAttributes(AudioAttributes.Builder().setContentType(AudioAttributes.CONTENT_TYPE_MUSIC).build()) //to send the object to the initialized state
 //            setDataSource(sampleUrl) //to set media source and send the object to the initialized state
 //            prepare() //to send the object to the prepared state, this may take time for fetching and decoding
 //            start() //to start the music and send the object to started state
 //        }
+
         }
 
 
