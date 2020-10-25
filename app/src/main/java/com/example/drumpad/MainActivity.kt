e5a9e73a7f6332bcb9f0663d9ab54d1674d304ec
RIIDLEY <hamsek.lahoucine@gmail.com>
2020-10-25 16:37:16 +0100

    Amelioration du xml et connection entre la page d'acceuil et le page login

--- app/src/main/java/com/example/drumpad/MainActivity.kt
+++ app/src/main/java/com/example/drumpad/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.drumpad
 
+import android.content.Intent
 import android.media.AudioManager
 import android.media.MediaPlayer
 import android.media.SoundPool
@@ -17,15 +18,21 @@ class MainActivity : AppCompatActivity() {
     private var sp: SoundPool? = null
     private val soundId = 2
     var loaded = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         var mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.cymbal1)
-
         sp = SoundPool(2, AudioManager.STREAM_MUSIC, 1)
         sp!!.load(applicationContext, R.raw.cymbal1, 2)
 
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main)
+
+            login.setOnClickListener {
+                val intent = Intent(this, FtpServer::class.java)
+                startActivity(intent)
+            }
+
 
 //            btn2.setOnClickListener {
 //                mediaPlayer?.start()
--- app/src/main/java/com/example/drumpad/ftpserver.java
+++
@@ -1,4 +0,0 @@
-package com.example.drumpad;
-
-public class ftpserver {
-}
