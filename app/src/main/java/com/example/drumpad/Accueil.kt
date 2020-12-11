59052df6e8f26022f97980ec541e1bd20b2b0752
RIIDLEY <hamsek.lahoucine@gmail.com>
2020-12-11 23:11:13 +0100

    Commu connection via log SharedPreferences

--- app/src/main/java/com/example/drumpad/Accueil.kt
+++ app/src/main/java/com/example/drumpad/Accueil.kt
@@ -1,15 +1,27 @@
 package com.example.drumpad
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.preference.PreferenceManager
+import com.android.volley.AuthFailureError
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_accueil.*
+import java.util.HashMap
 
 class Accueil : AppCompatActivity() {
 
     lateinit var sharedPreferences: SharedPreferences
+    val serverAPIURL: String = "http://lahoucine-hamsek.site/Drumpad.php"
+    var volleyRequestQueue: RequestQueue? = null
+    lateinit var progressDialog: ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -35,14 +47,54 @@ class Accueil : AppCompatActivity() {
 
         commu.setOnClickListener {
             if (sharedPreferences.getString("Login","")?.isNotEmpty()!!){//Si a deja ete log
-                val intent = Intent(this, Commu::class.java)
-                startActivity(intent)
+                progressDialog = ProgressDialog(this)
+                progressDialog.setTitle("Connection")
+                progressDialog.setMessage("En cours de connection")
+                progressDialog.show()
+                toServerLogin(sharedPreferences.getString("Login","")!!,sharedPreferences.getString("Pass","")!!)
             }else{//sinon il vas se log
                 val intent = Intent(this, Enregistrement::class.java)
                 startActivity(intent)
             }
         }
 
+    }
+
+    fun toServerLogin(pseudo: String, mdp: String){
+        volleyRequestQueue = Volley.newRequestQueue(this)
+        val parameters: MutableMap<String, String> = HashMap()
+        parameters.put("pseudo",pseudo)
+        parameters.put("mdp",mdp)
+        parameters.put("fonction","login")
+        val strReq: StringRequest = object : StringRequest(
+            Method.POST,serverAPIURL,
+            Response.Listener { response ->
+                Log.i("toServeur", "Send")
+                Log.i("reponse", response)
+                if(response=="OK"){
+                    progressDialog.dismiss()
+                    val intent = Intent(this, Commu::class.java)
+                    startActivity(intent)
+                }else{
+                    Toast.makeText(this, "Probleme de connection", Toast.LENGTH_SHORT).show()
+                }
+            },
+            Response.ErrorListener { volleyError -> // error occurred
+                Log.i("toServeur", "Error")}) {
+
+            override fun getParams(): MutableMap<String, String> {
+                return parameters;
+            }
+
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                val headers: MutableMap<String, String> = HashMap()
+                // Add your Header paramters here
+                return headers
+            }
+        }
+        // Adding request to request queue
+        volleyRequestQueue?.add(strReq)
     }
 }
 
